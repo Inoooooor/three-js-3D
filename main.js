71eb84d365d3008b58af3ca0bd2a39b1f4f99f56
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2024-08-16 13:13:32 +0300

    деплой

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import "./style.css"
 import * as THREE from "three"
 
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import space from "./space.jpg"
+import moonPhoto from "./moon.jpg"
+import moonNormal from "./normal.jpg"
+import ainurPhoto from "./ainur.jpg"
 
 const scene = new THREE.Scene()
 
@@ -47,42 +51,37 @@ function addStar() {
 
 Array(200).fill().forEach(addStar)
 
-const spaceTexture = new THREE.TextureLoader().load("space.jpg")
+const spaceTexture = new THREE.TextureLoader().load(space)
 scene.background = spaceTexture
 
-const ainurTexture = new THREE.TextureLoader().load("ainur.jpg")
+const ainurTexture = new THREE.TextureLoader().load(ainurPhoto)
 
 const ainur = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: ainurTexture }))
 scene.add(ainur)
 
-const moonTexture = new THREE.TextureLoader().load("moon.jpg")
-const normalTexture = new THREE.TextureLoader().load("normal.jpg")
+const moonTexture = new THREE.TextureLoader().load(moonPhoto)
+const normalTexture = new THREE.TextureLoader().load(moonNormal)
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
   new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: normalTexture })
 )
 scene.add(moon)
 
-moon.position.z = 30;
-moon.position.setX(-10);
-
+moon.position.z = 30
+moon.position.setX(-10)
 
 function moveCamera() {
+  const t = document.body.getBoundingClientRect().top
+  moon.rotation.x += 0.05
+  moon.rotation.y += 0.075
+  moon.rotation.z += 0.05
 
-  const t = document.body.getBoundingClientRect().top;
-  moon.rotation.x += 0.05;
-  moon.rotation.y += 0.075;
-  moon.rotation.z += 0.05;
+  ainur.rotation.y += 0.01
+  ainur.rotation.z += 0.01
 
-  ainur.rotation.y += 0.01;
-  ainur.rotation.z += 0.01;
-
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.rotation.y = t * -0.0002;
-
-
-
+  camera.position.z = t * -0.01
+  camera.position.x = t * -0.0002
+  camera.rotation.y = t * -0.0002
 }
 
 document.body.onscroll = moveCamera
